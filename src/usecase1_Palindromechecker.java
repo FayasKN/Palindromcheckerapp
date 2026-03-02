6fdc18476edab35d28a4521cb42f3fdd1951dacd
FayasKN <fayasfaya6677@gmail.com>
2026-03-02 09:05:34 +0530

    Base Application Setup

---
+++ src/usecase1_Palindromechecker.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class usecase1_Palindromechecker {
+public static void main(String[]args){
+    System.out.println("Welcome to Palindrome Checker Manager");
+    System.out.println("Version : 1.0");
+    System.out.println("System initialised Successfully");
+
+}
+}
+
