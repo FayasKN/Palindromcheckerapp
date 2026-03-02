285195e737fbbf7c90d8d5a54406f8c32287c16d
FayasKN <fayasfaya6677@gmail.com>
2026-03-02 09:44:20 +0530

    UC2:Hardcoded pallindrome

--- src/usecase1_Palindromechecker.java
+++ src/usecase1_Palindromechecker.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 import java.util.Stack;
 public class usecase1_Palindromechecker {
     public static void main(String[]args){
-        System.out.println("Welcome to Palindrome Checker Manager");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialised Successfully");
+        String word = "madam";
+        boolean ispalindrome = true;
 
+
+        for(int i = 0; i < input.length () / 2; i++){
+            if(word.charAt(i) != word.charAt(word.length()-1-i)){
+                ispalindrome=false;
+                break;
+            }
+        }
+        if(ispalindrome) {
+            System.out.println("Is it a Pallindrome ? yes ");
+        } else{
+            System.out.println("Is it a Pallindrome ? NO")
+        }
     }
 }
